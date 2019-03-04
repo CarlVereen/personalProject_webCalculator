079c18b889939a318d50bf79851bb8f49be62e75
Carl <carlvereen@gmail.com>
2019-03-04 10:27:06 -0700

    added Keyboard listners
    upgraded calculator to use keyboard commands, adjusted code to be uniform for keyboard values and html values.

--- calculatorBasicJS_v2.js
+++ calculatorBasicJS_v2.js
@@ -4,27 +4,56 @@ let recordButtonPressed = [];
 let calculationNumber = ['num1', 'num2'];
 let calculationOperation = ['oper1', 'oper2'];
 let calcMemory = 0;
+const numberKeyCodeArray = ['Digit0', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9' ];
+const operationKeyCodeArray = ['+', '-', '*', '/', 'Enter', 'NumpadEnter']
 theCalculator.addEventListener("click", buttonPressed, false);
-
 const currentNumber = () => { return parseFloat(displayContent.innerHTML) };
 const currentNumberString = () => { return displayContent.innerHTML };
+
+document.addEventListener('keyup', function (event) {
+  if (event.defaultPrevented) {
+    return;
+}
+  let key = event.key || event.keyCode;
+
+  if (operationKeyCodeArray.indexOf(key) > -1){
+        whichCommandRequested(key);
+        console.log(`it was an operation`);
+
+  }else if ((key >=0 && key <= 9) || numberKeyCodeArray.indexOf(key) > -1) {
+        console.log(`it was a number key`);
+        keyboardWhatToDoWithNumber(key);
+    }
+  recordButtonPressed.push(key);
+});
+
+
+
 
 //what button has been pushed
 function buttonPressed (button) {
   if (isNaN(button.target.value)){
-    whichCommandRequested(button)
+    getValueOfClick(button);
   }else {
-    whatToDoWithNumber(button)
+    screenWhatToDoWithNumber(button);
   }
   recordButtonPressed.push(button.target.value);
 }
 
 // number button pushed what do we want to do with it
-function whatToDoWithNumber(numberButton) {
+function screenWhatToDoWithNumber(numberButton) {
   if(isNaN(parseInt(recordButtonPressed[recordButtonPressed.length-1])) && recordButtonPressed[recordButtonPressed.length-1] != '.' ){
     startNewNumber(numberButton.target.value);
   } else {
     addToCurrentNumber(numberButton.target.value);
+  }
+}
+
+function keyboardWhatToDoWithNumber(numberButton) {
+  if(isNaN(parseInt(recordButtonPressed[recordButtonPressed.length-1])) && recordButtonPressed[recordButtonPressed.length-1] != '.' ){
+    startNewNumber(numberButton);
+  } else {
+    addToCurrentNumber(numberButton);
   }
 }
 
@@ -44,38 +73,48 @@ function mutateDisplay(number){
   displayContent.innerHTML = number;
 }
 
+function getValueOfClick (buttonClicked) {
+  whichCommandRequested(buttonClicked.target.value);
+}
+
+function getValueOfKey (keyPressed) {
+  whichCommandRequested(keyPressed);
+}
+
 //check for result request, or start of calc request, or operation on current, or memory
 function whichCommandRequested(commandButton){
-  //let currentNumber = parseFloat(displayContent.innerHTML);
-  console.log(`command check called with ${commandButton.target.id}`);
-  switch(commandButton.target.id) {
-    case "divi":
-    case "timesX":
-    case "minus":
-    case "plus":
+  console.log(`this command was called ${commandButton}`);
+  switch(commandButton) {
+    case "/":
+    case "*":
+    case "-":
+    case "+":
       if (calculationNumber[0] == 'num1') {
         console.log(`the addition command was called and there is no num1 ${calculationNumber[0]}`);
-        calculationRequest('start', commandButton.target.id);
+        calculationRequest('start', commandButton);
       }else if (calculationNumber[0] != 'num1' && calculationOperation[0] == 'oper1') {
         console.log(`the addition command was called and there is a num1 ${calculationNumber[0]}`);
-        calculationRequest('chageOperator', commandButton.target.id);
-      }else if (calculationNumber[0] != 'num1' && calculationOperation[0] != 'oper1' && calculationOperation[0] != commandButton.target.id) {
+        calculationRequest('chageOperator', commandButton);
+      }else if (calculationNumber[0] != 'num1' && calculationOperation[0] != 'oper1' && calculationOperation[0] != commandButton) {
         console.log(`change operation requested`);
-         calculationRequest('chageOperator', commandButton.target.id);
+         calculationRequest('chageOperator', commandButton);
       }
       break;
-    case 'equal':
+    case 'Enter':
       //check for numbers and operator
+      console.log(`Enter command called successfully`);
       if (calculationNumber[0] == 'num1' || calculationOperation[0] == 'oper1') {
         // do nothing
         console.log(`no numbers ${calculationNumber[0]} or operators ${calculationOperation[0]} `);
       } else if (calculationNumber[0] != 'num1' && calculationOperation[0] == 'oper1'){
         console.log(`has numbers ${calculationNumber[0]} but no operators ${calculationOperation[0]} `);
       }else if (calculationNumber[0] != 'num1' && calculationOperation[0] != 'oper1'){
-        if(recordButtonPressed[recordButtonPressed.length-1] == 'equal') {
+        console.log(`has number and operator here is last button pressed ${recordButtonPressed[recordButtonPressed.length-1]}`);
+        if(recordButtonPressed[recordButtonPressed.length-1] == 'Enter') {
           calculationRequest('performOperationWithStored');
 
         }else{
+          console.log(`perform with current called`);
           calculationRequest('performOperationWithCurrent');
         }
       }
@@ -100,7 +139,7 @@ function whichCommandRequested(commandButton){
       break;
     case 'm-plus':
     case 'm-minus':
-      mutateCurrentCalcMemory(commandButton.target.id);
+      mutateCurrentCalcMemory(commandButton);
       break;
     case "period":
       currentNumberFloat();
@@ -124,7 +163,9 @@ function calculationRequest (requestType, operationRequest){
   }else if (requestType == 'chageOperator') {
     calculationOperation[0] = operationRequest;
   } else if (requestType == 'performOperationWithCurrent'){
+    console.log(`here is what is in the equation ${calculationNumber} with ${calculationOperation}`);
     calculationNumber[1] = currentNumber();
+    console.log(`here is what is in the equation ${calculationNumber} with ${calculationOperation}`);
     performCalculationRequest(calculationNumber[0], calculationOperation[0], calculationNumber[1]);
   }else if (requestType == 'clear') {
     calculationNumber[0] = operationRequest;
@@ -142,17 +183,17 @@ function performCalculationRequest (numberOne, operator, numberTwo) {
  // console.log(`this is the number ${numberOne} provided, this is the operator ${operator} and this is the other number ${numberTwo}`);
   let calc;
   switch (operator) {
-    case 'plus':
+    case '+':
       calc = numberOne + numberTwo;
       break;
-    case 'minus':
+    case '-':
       calc = numberOne - numberTwo;
       break;
-    case 'timesX':
+    case '*':
       calc = numberOne * numberTwo;
       //console.log(`times was called and this is the answer ${calc}`);
       break;
-    case 'divi':
+    case '/':
       calc = numberOne / numberTwo;
       break;
   }
